389e2d19acf48658b51534b2a254879e6ac9c7e0
sraman samanta <sramansamanta2023@gmail.com>
2024-04-01 15:00:10 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import { Button } from '@material-tailwind/react'
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Button className='bg-red-500'>Close</Button>
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/sign-in' element={<SignIn/>}/>
+        <Route exact path='/sign-up' element={<SignUp/>}/>
+        <Route exact path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
